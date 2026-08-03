df9d0afc44bda26712392245f13cc0e39896754b
Gerald <geraldvillarp@gmail.com>
2026-08-03 20:17:59 +0800

    feat: add password strength checker and fix icon alignment

--- landingPageOsimplified.js
+++ landingPageOsimplified.js
@@ -232,6 +232,57 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    //PASSWORD STRENGTH FUNCTION//
+    const checkPasswordStrength = (password) => {
+        let score = 0; 
+        if (password.length >= 6) score++; 
+        if(password.length >= 10) score++;
+        if(/[A-Z]/.test(password)) score++; 
+        if(/[a-z]/.test(password)) score++;
+        if(/[0-9]/.test(password)) score++; 
+        if(/[!@#$%^&*]/.test(password)) score++; 
+
+        if(password.length === 0) {
+            return {message : "", color: ""};
+        }
+        
+        if(score <= 2){
+            return {message: "Weak password 😡", color: "red"};
+        }
+
+        else if(score === 3 || score === 4){
+            return {message: "Medium password 🤨", color: "orange"}; 
+        } 
+
+        else if(score === 5) {
+            return{message: "Strong password ✌", color: "blue"};
+        }
+
+        else {
+            return{message: "Excellent password 👌", color: "green"};
+        }
+    }
+
+    //PASSWORD STRENGTH TEST//
+
+    Object.entries(passwordInputs).forEach(([key, input]) => {
+        if(!input) return;
+
+        input.addEventListener("input", () => {
+
+            const strength = checkPasswordStrength(input.value);
+
+            //target message container per input//
+            const msg = document.querySelector(`[data-strength="${key}"]`);
+
+            if(!msg) return;
+
+            msg.textContent = strength.message;
+            msg.style.color = strength.color;
+        });
+    });
+
+
     //AFTER LOGGING IN LOGIC//
     //Login Authentication using local storage//
 
